4cfb4e3bb7af13a2dd82ef046c4eacfd2821e8e5
affmde <affmde@hotmail.com>
2023-10-09 14:42:12 +0300

    eslint added

--- frontend/src/Components/Homepage.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Homepage = ()=> {
-	return (
-		<div>
-			<h1>Ft-transcendence</h1>
-		</div>
-	)
-}
---
+++ frontend/src/Components/homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import "./Homepage.css";
+
+export const Homepage = () : JSX.Element=> {
+	const hello = "Hello"; 
+	console.log(hello);
+	return (
+		<div>
+			<h1>Ft-transcendence</h1>
+			<p>André - Carlos - Hans - João - Lucas</p>
+		</div>
+
+	);
+};
