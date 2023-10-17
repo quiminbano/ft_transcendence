a2be57501d2538855ca95233a9c9294db7a3efdf
Andre Filipe Ferreira Miranda <andrferr@c2r5p11.hive.fi>
2023-10-17 15:20:21 +0300

    SignUp component split. added responsiveness to it

--- frontend/src/Components/LoginPage/SignIn.tsx
+++ frontend/src/Components/LoginPage/SignIn.tsx
@@ -1,50 +1,90 @@
+import "./SignIn.css";
 import { InfoToSignProps } from "./Login";
 
 export const SignIn = (props: InfoToSignProps) : JSX.Element => {
+
+	return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-12">
+					<h2>Sign in to Ft-Transcendence</h2>
+				</div>
+			</div>
+			<div className="row mt-5">
+				<ButtonFor42Register />
+				<div className="col-12 col-md-8 offset-md-2">
+					<SignInInputs isLogin={props.isLogin} setIsLogin={props.setIsLogin}/>
+					<SignInButton />
+					<ForgotPasssword />
+				</div>
+				<SwitcherPanel isLogin={props.isLogin} setIsLogin={props.setIsLogin} />
+			</div>
+		</div>
+	);
+};
+
+export const ButtonFor42Register = () : JSX.Element => {
+	return (
+		<div className="col-12">
+			<div className="row">
+				<div className="col-12 col-md-8 offset-md-2">
+					<button className="btn btn-outline-secondary w-100">
+						<img alt="42 school logo" id="logo-42-school" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/800px-42_Logo.svg.png"></img>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const SignInInputs = (props: InfoToSignProps) : JSX.Element => {
 	const changeToSignInForm = () => {
 		props.setIsLogin(false);
 	};
 	return (
-		<div className="row d-flex align-items-center justify-content-center mt-5 mb-5">
-			<div className="col-10 offset-1 form-area d-flex flex-column align-items-center justify-content-center">
-				<div className="row login-form-title">
-					<h2 className="col-12 ">Sign in to Ft-Transcendence</h2>
+		<div className="row mt-5">
+			<div className="col-12">
+				<p>or use your email account</p>
+				<div className="input-group mb-3">
+					<input type="text" className="form-control" placeholder="Username" />
 				</div>
-				<div className="row">
-					<form className="col-10 offset-1">
-						<div className="row mt-4">
-							<input
-								className="col-10 my-0.2 my-md-1 form-control"
-								placeholder="Username"
-								type="text"
-							/>
-							<input
-								className="col-10 my-1 form-control"
-								placeholder="Password"
-								type="password"
-							/>
-						</div>
-						<div className="row mt-0.5 mt-md-3">
-							<div className="col-12 col-md-8 offset-md-2">
-								<a href="#">Forgot your password?</a>
-								<button className="mt-4 btn btn-success w-100">SIGN IN</button>
-							</div>
-						</div>
-						<div className="col-10 offset-1 mt-3">
-							<div className="col-8 offset-2">
-								<p className="mb-0">or</p>
-								<button className="btn btn-info w-100">Sign with 42</button>
-							</div>
-						</div>
-						<div className="small-screen-change-register row mt-4 mb-1">
-							<div className="col-12">
-								<button onClick={()=>changeToSignInForm()} className="btn btn-secondary">No account? Register here</button>
-							</div>
-						</div>
-					</form>
+				<div className="input-group mb-3">
+					<input type="password" className="form-control" placeholder="Password" />
 				</div>
 			</div>
+		</div>
+	);
+};
 
+const SignInButton = () : JSX.Element => {
+	return (
+		<div className="row">
+			<div className="col-12">
+				<button className="btn btn-success w-100">SIGN IN</button>
+			</div>
+		</div>
+	);
+};
+
+const ForgotPasssword = () : JSX.Element => {
+	return (
+		<div className="row mt-3">
+			<div className="col-12">
+				<button className="btn btn-link">Forgot your password?</button>
+			</div>
+		</div>
+	);
+};
+
+const SwitcherPanel = (props: InfoToSignProps) : JSX.Element => {
+	return (
+		<div className="row only-phone mt-4">
+			<div className="col-8 offset-2">
+				<button
+					onClick={()=>props.setIsLogin(false)}
+					className="btn btn-secondary w-100">Not registered? Sign up here
+				</button>
+			</div>
 		</div>
 	);
 };
--- frontend/src/Components/LoginPage/SignUp.tsx
+++ frontend/src/Components/LoginPage/SignUp.tsx
@@ -1,53 +1,86 @@
 import { InfoToSignProps } from "./Login";
+import { ButtonFor42Register } from "./SignIn";
 
 export const SignUp = (props: InfoToSignProps) : JSX.Element => {
+
+	return (
+		<div className="container-fluid">
+			<SignUpTitle />
+			<div className="row my-4">
+				<div className="col-12 col-md-10 offset-md-1">
+					<p>Register with</p>
+					<ButtonFor42Register />
+				</div>
+			</div>
+			<div className="col-12 col-md-10 offset-md-1 d-flex align-items-center justify-content-center">
+				<div className="row">
+					<div className="col-12 col-md-10 offset-md-1">
+						<p>or</p>
+						<SignUpForm isLogin={props.isLogin} setIsLogin={props.setIsLogin} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const SignUpTitle = () : JSX.Element => {
+	return (
+		<div className="row">
+			<div className="col-12 mb-3">
+				<h2>Create account</h2>
+			</div>
+		</div>
+	);
+};
+
+const SignUpForm = (props: InfoToSignProps) : JSX.Element => {
+	return (
+		<>
+			<div className="row">
+				<div className="col-12 col-md-10 offset-md-1 my-1">
+					<input
+						className=" form-control form-control"
+						placeholder="Username"
+						type="text"
+					/>
+				</div>
+				<div className="col-12 col-md-10 offset-md-1 my-1">
+					<input
+						className="form-control form-control"
+						placeholder="Email"
+						type="email"
+					/>
+				</div>
+				<div className="col-12 col-md-10 offset-md-1 my-1">
+					<input
+						className="form-control form-control"
+						placeholder="Password"
+						type="password"
+					/>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-12 col-md-10 offset-md-1">
+					<button className="mt-4 btn btn-success w-100">SIGN UP</button>
+				</div>
+			</div>
+			<div className="row">
+				<AlreadyRegistered isLogin={props.isLogin} setIsLogin={props.setIsLogin} />
+			</div>
+		</>
+	);
+};
+
+const AlreadyRegistered = (props: InfoToSignProps) : JSX.Element => {
 	const changeToSignInForm = () => {
 		props.setIsLogin(true);
 	};
 	return (
-		<div className="row d-flex align-items-center justify-content-center">
-			<div className="col-12 mb-3">
-				<h2>Create account</h2>
-			</div>
-			<div className="col-10 offset-1 d-flex align-items-center justify-content-center">
-				<div className="row">
-					<div className="col-10 offset-1">
-						<form>
-							<div className="row">
-								<div className="col-10 offset-1 my-1">
-									<input
-										className=" form-control form-control"
-										placeholder="Username"
-										type="text"
-									/>
-								</div>
-								<div className="col-10 offset-1 my-1">
-									<input
-										className="form-control form-control"
-										placeholder="Email"
-										type="email"
-									/>
-								</div>
-								<div className="col-10 offset-1 my-1">
-									<input
-										className="form-control form-control"
-										placeholder="Password"
-										type="password"
-									/>
-								</div>
-							</div>
-							<div className="row">
-								<div className="col-10 offset-1">
-									<button className="mt-4 btn btn-success w-100">SIGN UP</button>
-								</div>
-							</div>
-							<div className="small-screen-change-register row">
-								<button onClick={()=>changeToSignInForm()} className="mt-4 btn btn-secondary w-80">Already registered? Login here</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
+		<div className="phone-only col-12">
+			<button
+				onClick={()=>changeToSignInForm()}
+				className="mt-4 btn btn-secondary w-80">Already registered? Login here</button>
 		</div>
 	);
 };
