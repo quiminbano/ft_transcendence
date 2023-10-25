c39483225159675a9973daf606ef504ac7563a56
affmde <affmde@hotmail.com>
2023-10-25 09:54:13 +0300

    Circle progress bar moved to own component

--- frontend/src/Components/Dashboard/Profile.tsx
+++ frontend/src/Components/Dashboard/Profile.tsx
@@ -5,19 +5,19 @@ export const Profile = () : JSX.Element => {
 	const { coalition } = useUser().user;
 	return (
 		<div className="row">
-			<div className="col-12 order-1 sm-order-0 col-md-4 m-auto">
+			<div className="col-12 order-1 order-md-0 col-md-4 m-auto">
 				<div className="row">
 					<div className="col-12 d-flex flex-column justify-content-center align-items-center">
 						<img
 							alt="coalition logo"
 							src="https://img.icons8.com/?size=160&id=2lvtNFoNbR7b&format=png"
-							className="coalition-logo"
+							className="coalition-logo m-auto"
 						/>
-						<p>{coalition || "coalition name"}</p>
+						<p className="m-auto">{coalition || "coalition name"}</p>
 					</div>
 				</div>
 			</div>
-			<div className="col-12 col-md-8 order-0 sm-order-1 d-flex align-items-center justify-content-center">
+			<div className="col-12 col-md-8 order-0 order-md-1 d-flex align-items-center justify-content-center">
 				<img
 					alt="profile picture"
 					src="https://img.icons8.com/?size=160&id=SHo9iDefm73R&format=png"
