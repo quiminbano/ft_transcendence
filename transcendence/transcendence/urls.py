d6af7691d58991fbb25aa4749850dd1565d30941
Hans Otto Elneff <elneff@pm.me>
2023-11-17 16:49:20 +0200

    Changed testapp uri

--- transcendence/transcendence/urls.py
+++ transcendence/transcendence/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("testapp/", include("testapp.urls")),
+    path("", include("testapp.urls")),
     path('admin/', admin.site.urls),
 ]
