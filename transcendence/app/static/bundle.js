118a10eaeb3367dff7c2bb4eae2cc6c9e0592fd3
Hans Otto Elneff <elneff@pm.me>
2023-11-18 11:11:59 +0200

    Added basic API usage example

--- transcendence/app/static/bundle.js
+++ transcendence/app/static/bundle.js
@@ -1,1 +1,8 @@
-console.log("We be java scripting")
+async function testAPI() {
+    const res = await fetch("/api/hello");
+    const res_json = await res.json();
+    console.log(res_json);
+}
+
+console.log("We be java scripting");
+testAPI();
