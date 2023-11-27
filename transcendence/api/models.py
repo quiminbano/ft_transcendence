a1ba22718fc5d3624520a8232178535d5945a751
Lucas Eklund <luckeeklund@gmail.com>
2023-11-27 16:45:27 +0200

    testing

--- transcendence/api/models.py
+++ transcendence/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
-# Create your models here.
+class MockUser(models.Model):
+   uuid = models.UUIDField(primary_key=True)
+   onlineStatus = models.BooleanField()
+   loginName = models.CharField(max_length=100)
+   loginPassword = models.CharField(max_length=100)
+   email = models.EmailField()
+
+
+class Friend(models.Model):
+   mockUser = models.ForeignKey(MockUser, on_delete=models.CASCADE)
+   uuid = models.UUIDField()
+   addedDate = models.DateTimeField()
--- transcendence/api/views.py
+++ transcendence/api/views.py
@@ -1,4 +1,35 @@
 from django.http import JsonResponse
+from django.core.serializers import serialize
+from api.models import models
+import json
+import os
 
 def hello(request):
-    return JsonResponse({'msg': 'world'})
+    print("1")
+    print("Current working directory:", os.getcwd())
+
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    # file_path = os.path.join(dir_path, 'userMock.json')
+    with open('userMock.json', 'r') as file:
+    # with open(file_path, 'r') as file:
+       mockData = json.load(file)
+    print("2")
+    
+    fetchedUser = {
+    #    'uuid': mockData['uuid'],
+    #    'onlineStatus': mockData['onlineStatus'],
+    #    'loginName': mockData['loginName'],
+    #    'loginPassword': mockData['loginPassword'],
+    #    'email': mockData['email'],
+       'friends': [
+           {
+               'uuid': friend['uuid'],
+               'addedDate': friend['addedDate']
+           } for friend in mockData['friends']
+       ]
+   }
+    print("3")
+    print(fetchedUser)
+    print("4")
+    # responseFromMockData = serialize()
+    return JsonResponse(fetchedUser)
