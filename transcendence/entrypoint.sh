b81606bdc62245c11b9d359ec57b5f64d502fdda
Carlos Orellana <corellan@c2r6p12.hive.fi>
2023-11-29 14:45:50 +0200

    unhealthy issue fixed

--- transcendence/entrypoint.sh
+++ transcendence/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+variable=0
+
 python manage.py collectstatic --noinput
 
 until pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER
 do
 	echo "Waiting for the database to be ready"
 	sleep 2
+	variable=$((variable + 1))
+	if [ $variable -eq 30 ]; then
+		echo "The container had reached the ammount of tries to connect to the database. Exiting!!"
+		exit 1
+	fi
 done
+
 echo "Database is ready to be used!!"
 
 python manage.py makemigrations app
