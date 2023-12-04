2a3db85686b9cb6419b596a0a8a94a28395f5f99
affmde <affmde@hotmail.com>
2023-12-04 21:02:50 +0200

    added update display on tournament. With add remove and edit functionality on frontend side

--- transcendence/app/static/js/localTournamentClass.js
+++ transcendence/app/static/js/localTournamentClass.js
@@ -30,52 +30,77 @@ class LocalTournament {
         };
         this.nextId++;
         this.players.push(newPlayer);
-        this.#updateCurrentPlayersText();
-        console.log(this.players);
         this.errorElement.innerHTML = "";
 		this.inputField.value = "";
-		this.#updateDisplay(name);
-        if (this.players.length >= this.totalPlayers) {
-            this.startButton.style.display = "flex";
-            this.addNewPlayerButton.style.display = "none";
-        }
+        this.#updateCurrentPlayersText();
+        this.#updateDisplay();
+        console.log(this.players);
     }
 
     removePlayer(id) {
-        if (this.player.length <= 0) return;
+        if (this.players.length <= 0) return;
         console.log(`id: ${id}`);
-        this.players = this.players.map(p => p.id !== id);
+        this.players = this.players.filter(p => p.id !== id);
         this.#updateCurrentPlayersText();
+        this.#updateDisplay();
     }
 	editPlayer(id, name) {
 		console.log(id, name)
 		const playerToEdit = this.players.find(p => p.id === id);
-		if (playerToEdit)
-			playerToEdit.name = name;
+        if (playerToEdit) {
+            playerToEdit.name = name;
+            this.#updateDisplay();
+        }
+
 	}
 
     getPlayers() { return this.players; }
     getCurrentAmountOfPlayers() { return this.players.length; }
     setErrorElement(elem) { this.errorElement = elem; }
     setPlayersDisplay(elem) { this.playersDisplay = elem }
-
+    #cleanDisplay() {
+        const div = document.getElementById("registeredPlayerBox");
+        while (div.firstChild)
+            div.removeChild(div.firstChild);
+    }
+    #populateDisplay(doc) {
+        const div = document.getElementById("registeredPlayerBox");
+        for (let i = 0; i < this.players.length; i++) {
+            const fragment = document.createDocumentFragment();
+            const tempDiv = document.createElement("div");
+            tempDiv.innerHTML = doc.body.innerHTML;
+            while (tempDiv.firstChild)
+                fragment.appendChild(tempDiv.firstChild);
+            fragment.getElementById("playerItem").setAttribute("data-id", this.players[i].id);
+            fragment.getElementById("index").innerHTML = i + 1;
+            fragment.getElementById("playerName").innerHTML = this.players[i].name;
+            const deleteIcon = fragment.querySelector('.unregisterUserButton[alt="Remove user button"]');
+            deleteIcon.addEventListener("click", () => this.removePlayer(this.players[i].id));
+            const editIcon = fragment.querySelector('.unregisterUserButton[alt="Edit user button"]')
+            editIcon.addEventListener("click", () => openRegisterPlayerModal({ isNew: false, id: this.players[i].id }));
+            div.appendChild(fragment);
+        }
+    }
+    #updateButtons() {
+        if (this.players.length >= this.totalPlayers) {
+            this.startButton.style.display = "flex";
+            this.addNewPlayerButton.style.display = "none";
+        } else {
+            this.startButton.style.display = "none";
+            this.addNewPlayerButton.style.display = "flex";
+        }
+    }
 	async #updateDisplay(name) {
 		const parser = new DOMParser();
-		const url = "/getDoc/registerPlayer";
+        const url = "/getDoc/registerPlayer";
 		try {
 			const response = await fetch(url);
 			const html = await response.text();
-			const doc = parser.parseFromString(html, 'text/html');
-			const content = doc.body.innerHTML;
-			const indexText = doc.getElementById("index");
-			indexText.innerHTML = this.nextId;
-			const nameText = doc.getElementById("playerName");
-			nameText.innerHTML = name;
-			const element = document.createElement("div");
-			element.innerHTML = content;
-			const div = document.getElementById("registeredPlayerBox");
-			div.appendChild(element);
-			this.nextId++;
+            const doc = parser.parseFromString(html, 'text/html');
+
+            this.#cleanDisplay();
+            this.#populateDisplay(doc);
+            this.#updateButtons();
 		} catch (error) {
 			console.log(error);
 		}
