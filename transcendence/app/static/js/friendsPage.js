510979669e8bf78935c951631037ce2170d225eb
Andre Filipe Ferreira Miranda <andrferr@c2r6p10.hive.fi>
2023-12-15 13:19:23 +0200

    added buttons and char to friend page

--- transcendence/app/static/js/friendsPage.js
+++ transcendence/app/static/js/friendsPage.js
@@ -1,4 +1,8 @@
-const loadFriendsPage = () => {
+const loadFriendsPage = async () => {
 	loadMenus();
+	const winpercentChart = new CircleChart(120, 120);
+	await winpercentChart.createFragment();
+	winpercentChart.setPercent(85);
+	winpercentChart.appendFragment("friendWinsPercentDiv");
 }
 
