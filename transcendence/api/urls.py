113941408609d202341706afb02457899d39a6a3
Carlos Orellana Contreras <109737382+quiminbano@users.noreply.github.com>
2024-01-15 12:53:51 +0200

    Update urls.py

--- transcendence/api/urls.py
+++ transcendence/api/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path("tournament", views.tournamentManager, name="tournament"),
 	path("tournament/<int:id>", views.tournamentManagerID, name="tournamentID"),
     path("tournament/player/<int:id>", views.tournamentManagerPlayerID, name="tournamentPlayerID"),
-
-    # path("tournament", views.tournament, name="tournament"),
-	# path("tournament/<int:id>", views.tournamentWithID, name="tournament"),
-    # path("tournament/player", views.tournamentPlayer, name="tournament/player"),
-	# path("tournament/player/<int:id>", views.tournamentPlayer, name="tournament/player"),
 ]
