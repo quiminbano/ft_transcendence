c0fdf547544450522c63d28ab6425dafec705492
Andre Filipe Ferreira Miranda <andrferr@c1r1p5.hive.fi>
2024-02-06 14:45:04 +0200

    changed the set42password

--- transcendence/app/static/js/set42password.js
+++ transcendence/app/static/js/set42password.js
@@ -1,18 +1,37 @@
-const save42password = async (e) => {
+const set42password = async (e) => {
 	e.preventDefault();
 	const url = "/api/getRestInfo";
 
 	const form = new FormData(e.target);
-	const password = form.get("password");
-	console.log(password);
+	const body = {
+		password1: form.get("password1"),
+		password2: form.get("password2")
+	}
+	console.log(body);
 	try {
-		const response = await postRequest(url, password);
+		const response = await postRequest(url, body);
 		if (response.succeeded) {
 			console.log(response);
 		} else {
 			throw response;
 		}
 	} catch(error) {
+		console.log(error);
+	}
+}
+
+const cancel42password = async (e) => {
+	e.preventDefault();
+	const url = "";
+
+	try {
+		const response = await deleteRequest(url);
+		if (response.succeeded) {
+			console.log(response);
+		} else {
+			throw response;
+		}
+	} catch (error) {
 		console.log(error);
 	}
 }
