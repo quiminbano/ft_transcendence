87cadf038b3d2d92be4c9017188733d4c2b24c09
Lucas Eklund <luckeeklund@gmail.com>
2024-02-11 08:59:11 +0200

    new models rewamp done

--- transcendence/api/tournamentController.py
+++ transcendence/api/tournamentController.py
@@ -35,43 +35,28 @@ def unknownMethod():
 #==========================================
 #       Tournament Player Functions                                                                                                                                           
 #==========================================
-def tournamentAddPlayer(request, tournament):
+def tournamentAddPlayer(request):
     data = json.loads(request.body)
-    if "player" not in data:
+    if "player" not in data or "team" not in data:
         return JsonResponse({'error': 'missing fields in request body'}, status=400)
-    player = Players.objects.create(name=data['player'])
-    tournament.players.add(player)
-    playerdict = model_to_dict(player)
-    if tournament.players.count() >= tournament.amount:
-        tournament.state = 'A'
-        tournament.save()
-    return JsonResponse({'message': 'Player added successfully', 'player': playerdict}, status=200)
+    player = Database.objects.filter(username=data['player'])
+    if player is None:
+        return JsonResponse({'error': 'player not found'}, status=400)
+    team = Team.objects.filter(id=data['team'])
+    if (team is None):
+        return JsonResponse({'error': 'team not found'}, status=400)
+    team.players.add(player)
+    return JsonResponse({'message': 'Player added successfully', 'player': player.username}, status=200)
 
-def tournamentUpdatePlayer(request):
-    data = json.loads(request.body)
-    if "id" not in data or "username" not in data:
-        return JsonResponse({'error': 'missing fields in request body'}, status=400)
-    player = Players.objects.filter(id=data['id']).first()
-    if player is not None:
-        player.name =  data["username"]
-        player.save()
+def tournamentDeletePlayer(existing_tournament, player):
+
+    if existing_tournament.match.first().team1.players.filter(player).exist():
+        existing_tournament.match.first().team1.players.filter(player).remove()
+    elif existing_tournament.match.first().team2.players.filter(player).exist():
+        existing_tournament.match.first().team2.players.filter(player).remove()
     else:
         return JsonResponse({'error': 'player does not exist'}, status=400)
-    playerdict = model_to_dict(player)
-    return JsonResponse({'message': 'Player name change succefull', 'player': playerdict}, status=200)
-
-def tournamentDeletePlayer(player):
-    tournament = Tournament.objects.filter(players=player).first()
-    if tournament is not None:
-        tournament.players.remove(player)
-        player.delete()
-        if tournament.players.count() < tournament.amount:
-            tournament.state = 'P'
-            tournament.save()
-        return JsonResponse({'succes!': 'player got removed'}, status=200)
-    else:
-        return JsonResponse({'error': 'player or tournament does not exist'}, status=400)
-
+    return JsonResponse({'succes!': 'player got removed'}, status=200)
 
 #==========================================
 #         Tournament Functions                                                                                                                                           
@@ -80,8 +65,6 @@ def createTurnament(request):
     data = json.loads(request.body)
     if "name" not in data or "number" not in data:
         return JsonResponse({'error': 'missing fields in request body'}, status=400)
-    print (data)
-    #Create a tournament object and add the player to it
     tournament = Tournament.objects.create(tournament_name=data['name'], teams=data['number'])
     tournament.matches.add(
         Match.objects.create(
@@ -89,29 +72,20 @@ def createTurnament(request):
             team2=Team.objects.create()
 	))
     tournament.matches.first().team1.players.add(request.user)
-    print(tournament.tournament_name)
     return JsonResponse(JSONTournamentResponse(tournament, "Tournament created successfully"), status=200)
 
 def getTournament(tournament):
     return JsonResponse(JSONTournamentResponse(tournament, "Tournament already exist"), status=200)
 
 
-#
-#
-#
-#
-#       TEMPORARY ADD MATCHES TO COMPLETED COLUMN IF THEY ARE ACTIVE CHANGE THIS TO BE BETTER MANAGE!!!!!
-#                                               (READ!!!)
-#
-#
-#
 def deleteTournament(request, tournament):
     if tournament.completed == True:
         print("MOVE")
         request.user.completed_matches.add(tournament)
-        tournament.delete()
     else:
         print("DELE")
+        for match in tournament.matches.all():
+            match.delete()
         tournament.delete()
     return JsonResponse({'message': 'Succefully delete tournament'}, status=200)
 
@@ -167,33 +141,18 @@ def tournamentManagerID(request, id=None):
         return JsonResponse({'error': 'User does not have an tournament going'}, status=404)
 
 #==========================================
-#         Tournament Player Management
-#==========================================
-def tournamentPlayer(request):
-    data = json.loads(request.body)
-    print(data)
-    print(request.method)
-    match request.method:
-        case "PUT":
-            return(tournamentUpdatePlayer(request))
-        case _:
-            return unknownMethod()
-
-
-
-
-#==========================================
 #         Tournament Player ID Management
 #==========================================
 def tournamentManagerPlayerID(request, id=None):
-    player = Players.objects.filter(id=id).first()
-    if player is not None:
+    player = Database.objects.filter(id=id).first()
+    existing_tournament = request.user.tournament
+    if player is not None or existing_tournament is not None:
         match request.method:
             case "DELETE":
-                return tournamentDeletePlayer(player)
+                return tournamentDeletePlayer(existing_tournament, player)
             case _:
                 return unknownMethod()
     else:
-        return JsonResponse({'error': 'player does not exist'}, status=400)
+        return JsonResponse({'error': 'something went wrong'}, status=400)
 
 
