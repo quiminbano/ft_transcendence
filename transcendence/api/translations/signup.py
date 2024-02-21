1ca9fdb5ab2ed524c7df365b667b87d38aa95d3f
Carlos Orellana <corellan@c1r1p4.hive.fi>
2024-02-21 09:19:36 +0200

    signup translated, also the buttons

---
+++ transcendence/api/translations/signup.py
@@ -0,0 +1,62 @@
+dictionary = {
+    "englishButton": {
+        "eng": "English",
+        "fin": "Englantti",
+        "swe": "Engelska"
+    },
+    "finnishButton": {
+        "eng": "Finnish",
+        "fin": "Suomi",
+        "swe": "Finska"
+    },
+    "swedishButton": {
+        "eng": "Swedish",
+        "fin": "Ruotsi",
+        "swe": "Svenska"
+    },
+    "title": {
+        "eng": "Register",
+        "fin": "Rekisteröidy",
+        "swe": "Registrera"
+	},
+	"username": {
+		"eng": "Username:",
+		"fin": "Käyttäjänimi:",
+		"swe": "Användarnamn:"
+	},
+    "email": {
+        "eng": "Email:",
+        "fin": "Sähköposti:",
+        "swe": "Epost:",
+	},
+    "coalition": {
+        "eng": "Coalition:",
+        "fin": "Koalitio:",
+        "swe": "Koalition:"
+	},
+	"password": {
+		"eng": "Password:",
+		"fin": "Salasana:",
+		"swe": "Lösenord:"
+	},
+    "passwordConfirmation": {
+        "eng": "Confirm password:",
+        "fin": "Vahvista salasana:",
+        "swe": "Bekrätfa lösenord:"
+    },
+	"registerButton": {
+        "eng": "Register",
+        "fin": "Rekisteröidy",
+        "swe": "Registrera"
+	},
+	"cancelButton": {
+		"eng": "Cancel",
+		"fin": "Peruuta",
+		"swe": "Avbryt"
+	},
+    "loginMessage": {
+        "eng": "Do you already have an account? Login here",
+        "fin": "Onko sinulla jo tili? Kirjaudu sisään täällä",
+        "swe": "Har du redan ett konto? Logga in här"
+	}
+}
