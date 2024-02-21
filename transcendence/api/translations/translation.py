25940c85954447f18790addbad4b5e2ac0b7879b
Andre Filipe Ferreira Miranda <andrferr@c1r1p5.hive.fi>
2024-02-21 13:26:06 +0200

    added back the translations to py

--- transcendence/api/translations/translation.py
+++ transcendence/api/translations/translation.py
@@ -10,6 +10,10 @@ from .tournamentBoard import dictionary as tournamentBoard
 from .login import dictionary as login
 from .games import dictionary as games
 from .signup import dictionary as signup
+from .settings import dictionary as settings
+from .usersPage import dictionary as usersPage
+from .password42 import dictionary as password42
+from .invitations import dictionary as invitations
 
 pages = {
     "main": main,
